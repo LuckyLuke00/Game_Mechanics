9a077bdc110f53ae062e8c3b404c80e591ad5578
LuckyLuke00 <Lucas.Kinoo5@gmail.com>
2022-10-15 18:00:36 +0200

    Implement basic enemy seek ai

--- 2GD15N_Kinoo_Lucas_Unity_Game/GMUnityGame/Assets/LucasKinoo/GMUnityGame/Scripts/MovementBehaviour.cs
+++ 2GD15N_Kinoo_Lucas_Unity_Game/GMUnityGame/Assets/LucasKinoo/GMUnityGame/Scripts/MovementBehaviour.cs
@@ -5,8 +5,7 @@ using UnityEngine.InputSystem;
 
 public class MovementBehaviour : MonoBehaviour
 {
-    [SerializeField]
-    private float _movementSpeed = 10f;
+    [SerializeField] protected float _movementSpeed = 10f;
 
     // Declare reference variables
     private CharacterController _characterController = null;
@@ -20,18 +19,25 @@ public class MovementBehaviour : MonoBehaviour
         set { _currentMovement = value; }
     }
 
-    private void Awake()
+    protected GameObject _target = null;
+    public GameObject Target
+    {
+        get { return _target; }
+        set { _target = value; }
+    }
+
+    protected virtual void Awake()
     {
         // Initially set reference variables
         _characterController = GetComponent<CharacterController>();
     }
 
-    void Update()
+    private void Update()
     {
         HandleMovement();
     }
 
-    private void HandleMovement()
+    protected virtual void HandleMovement()
     {
         _characterController.Move(_currentMovement * _movementSpeed * Time.deltaTime);
     }
